836508191b6f347bf30e759ecc64ab02f15e9ed2
Myles Broomes <mylesbroomes@hotmail.co.uk>
2022-11-28 18:25:17 +0000

    Add webflow files

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
-    if ($('body').hasClass('home')) {
-        console.log('HOME')
-        $('#home-link').addClass('active')
-    } else if ($('body').hasClass('services')) {
-        $('#services-link').addClass('active')
-    }
-})
